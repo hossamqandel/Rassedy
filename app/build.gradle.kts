5d0678ee6b4305b602603f266588d33c58f88fc3
Hossam <hossam.sayed@passboard.net>
2023-08-31 07:56:06 +0300

    feature: Upgrade target SDK to 34 (Android 14)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "dev.hossam.raseedy"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "dev.hossam.raseedy"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
