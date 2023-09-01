41f53ac1fefa26a1dabed736e5459f42fa0f088a
Hossam <hossam.sayed@passboard.net>
2023-09-01 17:03:40 +0300

    refactor: Remove unused space

--- app/src/main/java/dev/hossam/raseedy/MainActivity.kt
+++ app/src/main/java/dev/hossam/raseedy/MainActivity.kt
@@ -35,7 +35,6 @@ class MainActivity : ComponentActivity() {
             }
             RaseedyTheme {
 
-
                 Surface(modifier = Modifier.fillMaxSize()) {
                     Splash()
                 }
